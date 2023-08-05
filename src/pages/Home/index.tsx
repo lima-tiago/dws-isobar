3bdc8b37fdad8474e2e414028fa462bdecd97aa6
Tiago Lima <tiago.lima@oli.live>
2023-08-05 18:00:02 -0300

    First commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as Style from "./styles";
+
+export const Home = () => {
+  return (
+    <Style.Main>
+      <div className="container">
+        <h1>Home</h1>
+      </div>
+    </Style.Main>
+  );
+};
